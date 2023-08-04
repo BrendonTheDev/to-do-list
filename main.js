1b62ffe3041d6f21194aa07de7b49618e1bbd620
brendon <200~108491991+BrendonTheDev@users.noreply.github.com>
2023-08-04 16:21:34 -0400

    Set projectID local storage key to save() function Selected Id now persists after refresh.
    Add delete Project Button.  OnClick filters Projects Array to return
    all except the selected project.
    Sets selectedId to null.
    
    Now moving to
    to-do section until
    need
    logic for rendering different projects onclick.

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 // 1. Access html and store and ad vars to use in JS
-const projectListContainer = document.querySelector('[data-project-container]');
+const projectContainer = document.querySelector('[data-project-container]');
 const newProjectForm = document.querySelector('[data-new-project-form]');
 const newProjectInput = document.querySelector('[data-new-project-input]');
+const deleteProjectButton = document.querySelector('[data-delete-project-button]');
 
-// 1. Project Input Data is stored as an obj with Id and name properties
+// 1. Local Storage keys
 const LOCAL_STORAGE_PROJECT_KEY = "task.projects"
+const LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY = "task.selectedProjectId"
+
 // 1. Get the info from local storage using key 2. If any projects exist parse from string into object
 // 3. Or if it doesn't exist give us an empty array of projects to begin with. 
 let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || []
 
+let selectedProjectId = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY)
+
+// 1. If tag is an <li> assign the clicked li project.id to the selectedProjectId variable
+// 2. Call save and Render
+projectContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedProjectId = e.target.dataset.projectId
+        saveAndRender()
+    }
+})
+
+// 1. OnClick Filter to return all projects that are not selected
+// 2. Set selectedID to null because it no longer exists  3. Save To Local and rerender page/DOM
+deleteProjectButton.addEventListener('click', e => {
+    projects = projects.filter(project => project.id !== selectedProjectId)
+    selectedProjectId = null
+    saveAndRender()
+})
 
 // 1. Prevents default submit         2. Assigns input value to projectName 
 // 3. If no name is given do nothing  4. Call createProject() and assign to project Var
@@ -39,21 +60,26 @@ function saveAndRender() {
 // 1. Using local storage key , passes value of key and saves projects to local storage as JSON string. 
 function save() {
   localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projects))
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY, selectedProjectId)
 }
 
 
 // 1. Clears the ulContainer                         2. ForEach project in Projects array create a list DOM elem
 // 2. Acceess and add id to the elem using .notation 3. Add class to Elem
-// 4. Add project name from prop.value to innerText  5. Append newly created <li> to the ul ProjectListContainer
+// 4. Add project name from prop.value to innerText  5. If project.id equals selected id add class active-project
+// 6. Append newly created <li> to the ul projectContainer
 function renderProject() {
-  clearElement(projectListContainer)  
+  clearElement(projectContainer)  
   projects.forEach(project => {
     const listElement = document.createElement('li')
     // check if projectId needs to be listId
     listElement.dataset.projectId = project.id  
     listElement.classList.add('project-name')
     listElement.innerText = project.name
-    projectListContainer.appendChild(listElement)
+    if (project.id === selectedProjectId) {
+        listElement.classList.add('active-project')
+    }
+    projectContainer.appendChild(listElement)
   })
 }
 
@@ -76,15 +102,15 @@ renderProject()
 
 //modal logic
 
-const modal = document.querySelector('.modal');
-const openModal = document.querySelector('.add-task-btn');
-const closeModal = document.querySelector('.submit-task-btn');
+// const modal = document.querySelector('.modal');
+// const openModal = document.querySelector('.add-task-btn');
+// const closeModal = document.querySelector('.submit-task-btn');
 
-openModal.addEventListener('click', () => {
-    modal.style.display = "flex";
-})
+// openModal.addEventListener('click', () => {
+//     modal.style.display = "flex";
+// })
 
-closeModal.addEventListener('click', () => {
-    modal.style.display = "none";
-})
+// closeModal.addEventListener('click', () => {
+//     modal.style.display = "none";
+// })
 
